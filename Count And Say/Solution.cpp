667d4cd43a56b9ce7cd2fdcb00754436a226bef4
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-10-06 11:59:48 +0530

    CountAndSay

---
+++ Count And Say/Solution.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+
+        string prev = countAndSay(n - 1);
+        string result = "";
+        int count = 1;
+
+        for (int i = 0; i < prev.length(); ++i) {
+            if (i + 1 < prev.length() && prev[i] == prev[i + 1]) {
+                count++;
+            } else {
+                result += to_string(count) + prev[i];
+                count = 1;
+            }
+        }
+
+        return result;
+    }
+};
